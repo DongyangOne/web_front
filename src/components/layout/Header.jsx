384c91f575ebbe5fb59ccf35964b734c0e75e3c7
chlgusdn0203 <dudrjsdud@naver.com>
2026-06-08 14:14:09 +0900

    fix: 상단바 그림자, 색상 수정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 
 import { ROUTES } from '@/constants/routes';
+import { COLORS } from '@/constants/theme';
 import calendarBlackIcon from '@/assets/images/Date_today_duotone_line1.svg';    //검정 아이콘
 import calendarOrangeIcon from '@/assets/images/Date_today_duotone_line2.svg';  //주황 아이콘
 
@@ -9,7 +10,7 @@ import userOrangeIcon from '@/assets/images/User2.svg'; //주황 아이콘
 
 function Header() {
   return (
-    <header className="w-full bg-white shadow-[0_4px_12px_rgba(0,0,0,0.15)]">
+    <header className="w-full bg-white shadow-[0_4px_24px_rgba(0,0,0,0.35)]">
       <nav className="flex items-center justify-center gap-20 px-6 py-6">
         <NavLink to={ROUTES.SCHEDULE}>
   {({ isActive }) => (
@@ -25,11 +26,10 @@ function Header() {
       />
 
       <span
-        className={`text-xl font-medium ${
-          isActive
-            ? 'text-orange-500'
-            : 'text-black'
-        }`}
+        className="text-xl font-medium"
+        style={{
+          color: isActive ? COLORS.orange : '#000000',
+        }}
       >
         연간 계획
       </span>
@@ -51,11 +51,10 @@ function Header() {
       />
 
       <span
-        className={`text-xl font-medium ${
-          isActive
-            ? 'text-orange-500'
-            : 'text-black'
-        }`}
+        className="text-xl font-medium"
+        style={{
+          color: isActive ? COLORS.orange : '#000000',
+        }}
       >
         신입 부원 모집
       </span>
