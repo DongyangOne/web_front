21f26dd51543891079a40bd5813c9e811e7c12b1
takjinwu <takjinwu0613@gmail.com>
2026-06-14 22:46:03 +0900

    fix:상단바 그림자 적용

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,6 @@
 import { NavLink } from 'react-router-dom';
 
 import { ROUTES } from '@/constants/routes';
-import { COLORS } from '@/constants/theme';
 import calendarBlackIcon from '@/assets/images/Date_today_duotone_line1.svg';    //검정 아이콘
 import calendarOrangeIcon from '@/assets/images/Date_today_duotone_line2.svg';  //주황 아이콘
 
@@ -10,7 +9,7 @@ import userOrangeIcon from '@/assets/images/User2.svg'; //주황 아이콘
 
 function Header() {
   return (
-<header className="w-full bg-white shadow-header">
+<header className="relative z-10 w-full bg-white shadow-header">
         <nav className="flex items-center justify-center gap-20 px-6 py-6">
         <NavLink to={ROUTES.SCHEDULE}>
   {({ isActive }) => (
